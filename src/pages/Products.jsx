cad396387e29806e34648c88a18bff0588f30549
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-07-17 16:21:10 +0500

    Added Condition to display note when there are no products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,37 +1,40 @@
 import ProductThumbnail from '../components/ProductThumbnail'
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 const API_URL = process.env.REACT_APP_API_URL
 
 export default function Products() {
 // define state for updating the variable 
-const [products, setProducts] = useState ([])
-// define function to import data from the API
-async function getProducts() {
-  let productsData = await axios.get(`${API_URL}`)
-  setProducts(productsData.data)
-}
-useEffect(() => {getProducts()}, [])
-return (
+  const [products, setProducts] = useState ([])
+  // define function to import data from the API
+  async function getProducts() {
+    let productsData = await axios.get(`${API_URL}`)
+    setProducts(productsData.data)
+  }
+  useEffect(() => {getProducts()}, [])
+  return (
     <>
-    {/* Make a container for the create product button */}
-    <div className='container-fluid '>
+      {/* Make a container for the create product button */}
+      <div className='container-fluid '>
         {/* Add Create Product Button */}
-            <Link className="row justify-content-center" to="/createproduct"><button className="btn btn-primary col-4 m-4">Create Product</button>
-            </Link>
-        </div>
-    {/*  Make a container for the product cards grid */}
-    <div className="container-fluid ">
-      {/* Define row for the product cards grid */}
-      <div className="row">
-          {/* Map the array of products to each card to represent each product */}
-          {products.map((product, i) =>
-          // Display a card for each product. Render the Product Thumbnail component, pass props
-          <ProductThumbnail product={product} key={i} API_URL={API_URL} getProducts={getProducts} />
-          )}
+        <Link className="row justify-content-center" to="/createproduct"><button className="btn btn-primary col-4 m-4">Create Product</button>
+        </Link>
       </div>
-    </div>
+      {/* Make a conditional statement to see if the any products exist */}
+         
+      {/*  Make a container for the product cards grid */}
+      {Array.isArray(products) && products.length > 0 ?
+        (<div className="container-fluid ">
+          {/* Define row for the product cards grid */}
+          <div className="row">
+            {/* Map the array of products to each card to represent each product */}
+            {products.map((product, i) =>
+            // Display a card for each product. Render the Product Thumbnail component, pass props
+              <ProductThumbnail product={product} key={i} API_URL={API_URL} getProducts={getProducts} />
+            )}
+          </div>
+        </div> ) : (<h1 className="center-text">There are no Products Listed</h1>)} 
     </>  
-  );
+  )
 }
