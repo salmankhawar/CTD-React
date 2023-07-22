d6398eb805bb84868266f9abc1a95eddc0100bac
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-07-22 21:07:00 +0500

    alignment for buttons adjusted for XS devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,15 +5,15 @@ export default function Nav() {
   // create container for the Navigation bar
     <div className="container col-8">
       <div className='row m-2'>
-        <div className= "text-center m-2">
+        <div className= "text-center">
           {/* Add Home Button */}
-          <Link to="/"><button className="btn btn-primary col-12 col-sm-4 col-md-3 m-2">Home</button>
+          <Link to="/"><button className="btn btn-primary col-10 col-sm-4 col-md-3 m-2">Home</button>
           </Link>
           {/* Add Create Product Button */}
-          <Link to="/createproduct"><button className="btn btn-primary col-12 col-sm-5 col-md-4 m-2">Create Product</button>
+          <Link to="/createproduct"><button className="btn btn-primary col-10 col-sm-5 col-md-4 m-2">Create Product</button>
           </Link>
           {/* Add Walkhthrough Button */}
-          <Link to="/walkthrough"><button className="btn btn-primary col-12 col-sm-4 col-md-3 m-2">Walkthrough</button></Link>
+          <Link to="/walkthrough"><button className="btn btn-primary col-10 col-sm-4 col-md-3 m-2">Walkthrough</button></Link>
         </div>
       </div>
     </div>
